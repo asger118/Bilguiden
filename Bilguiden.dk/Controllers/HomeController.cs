ceaed9c36a4cefdbac775131a9ec7c5923995325
Asger Amtkjær Veng <asgerveng1@gmail.com>
2022-03-04 11:48:23 +0100

    Tilføj Bil til database

--- Bilguiden.dk/Controllers/HomeController.cs
+++ Bilguiden.dk/Controllers/HomeController.cs
@@ -11,10 +11,40 @@ namespace Bilguiden.dk.Controllers {
             return View();
         }
 
-        public ActionResult Biler() {
+        public ActionResult Biler() { //View med alle biler fra databasen
 
             List<Biler> AlleBiler = new List<Biler>();
 
+            return View();
+        }
+
+        public ActionResult TilføjBil() {   //Tilføj bil til database view
+            return View();
+        }
+        [HttpPost]
+        public ActionResult TilføjBil(TilføjBilModel NyBil) {
+            Biler Bil = new Biler();
+                Bil.Acceleration    = NyBil.Acceleration;
+                Bil.Drivmiddel      = NyBil.Drivmiddel;
+                Bil.Gearkasse       = NyBil.Gearkasse;
+                Bil.Hestekræfter    = NyBil.Hestekræfter;
+                Bil.Kubik           = NyBil.Kubik;
+                Bil.Lastevne        = NyBil.Lastevne;
+                Bil.Model           = NyBil.Model;
+                Bil.Motor           = NyBil.Motor;
+                Bil.Mærke           = NyBil.Mærke;
+                Bil.Newtonmeter     = NyBil.Newtonmeter;
+                Bil.Nypris          = NyBil.Nypris;
+                Bil.Træk            = NyBil.Træk;
+                Bil.Vægt            = NyBil.Vægt;
+                Bil.Årgang          = NyBil.Årgang;
+
+            using(bilguidenDBEntities x = new bilguidenDBEntities()) {
+                x.Biler.Add(Bil);
+                x.SaveChanges();
+            }
+
+            ViewBag.SavedData = true;
             return View();
         }
 
