aae665b2bc2a60b100c7070d66b8fdd84e4f116c
Asger Amtkjær Veng <asgerveng1@gmail.com>
2022-04-07 14:39:21 +0200

    update

--- Bilguiden.dk/Scripts/Script/Funktioner.js
+++ Bilguiden.dk/Scripts/Script/Funktioner.js
@@ -2,33 +2,57 @@
 let Biler = [];
 const buttons = document.querySelectorAll('.knap');
 
-
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            button.classList.add('active');
-        });
-    });
-
-
 function GetBil_ID(Bil_ID) {
-    if (Biler.length <= 2) {
+    if (Biler.length <= 3) {
         //Checker om Bil_ID er i arrayet i forvej
         if (Biler.includes(Bil_ID)) {
             //Hvis Bil_ID er i array[Biler]
-        } else {
+            } else {
         //Hvis Bil_ID ikke er i array[Biler]
-                    Biler.push(Bil_ID);
-                    console.log(Biler);
+                switch (Biler.length) {
+                    case 0:
+                        Biler.push(Bil_ID);
+                        console.log(Biler);
+                        break;
+                    case 1:
+                        Biler.push(Bil_ID);
+                        console.log(Biler);
+                        break;
+                    case 2:
+                        Biler.push(Bil_ID);
+                        console.log(Biler);
+                        break;
+                    case 3:
+                        Biler.length = 0;
+                        buttons.forEach(button => button.classList.remove('active'));
+                        Biler.push(Bil_ID);
+                        console.log(Biler);
+                        break;
+        }
         }
     }
 }
-    
-function FjernBil_ID() {
-    if (Biler.length > 0) {
-        Biler.length = 0;
-        console.log(Biler);
-    }
-}
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+
+        switch (Biler.length) {
+            case 1:
+                button.classList.add('active');
+                break;
+            case 2:
+                button.classList.add('active');
+                break;
+            case 3:
+                button.classList.add('active');
+                break
+            default:
+                button.classList.remove('active');
+                button.classList.add('active');
+        }
+    });
+});
+
 //URL: hvor dataen skal hen/endpoint
 function SendData() {
     window.location.href = "/Home/Sammenlign?Bil_ID1=" + Biler[0] + "&Bil_ID2=" + Biler[1]+ "&Bil_ID3=" + Biler[2];
